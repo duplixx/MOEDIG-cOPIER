3aaf96d8e2762ac9d0a2d40dbd51f24ed1d51546
duplixx <90516956+duplixx@users.noreply.github.com>
2022-04-06 23:42:57 +0530

    main

---
+++ js/reveal.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.hed1', { delay: 500 })
+ScrollReveal().reveal('.hed1-red', { delay: 600,origin:'right' })
+ScrollReveal({reset: true,distance:'60px',duration:200})
+ScrollReveal().reveal('.para', { delay: 700,origin:'bottom' })
+ScrollReveal({reset: true,distance:'60px',duration:200})
+ScrollReveal().reveal('.arrow', { delay: 800,origin:'left' })
+ScrollReveal().reveal('.hed1', { delay: 500 })
+ScrollReveal().reveal('.menu-ul li', { delay: 1000,origin:'right',interval:100})
+
+
