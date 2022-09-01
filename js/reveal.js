35a8870b6e3dad406ca98c7aa2c8bb7ef434c5d0
duplix <duplixxx20@gmail.com>
2022-09-01 18:10:01 +0530

    okay

--- js/reveal.js
+++ js/reveal.js
@@ -1,10 +1,13 @@
 ScrollReveal().reveal('.hed1', { delay: 500 })
 ScrollReveal().reveal('.hed1-red', { delay: 600,origin:'right' })
 ScrollReveal({reset: true,distance:'60px',duration:200})
-ScrollReveal().reveal('.para', { delay: 700,origin:'bottom' })
+ScrollReveal().reveal('.para', { delay: 400,origin:'bottom' })
 ScrollReveal({reset: true,distance:'60px',duration:200})
 ScrollReveal().reveal('.arrow', { delay: 800,origin:'left' })
 ScrollReveal().reveal('.hed1', { delay: 500 })
 ScrollReveal().reveal('.menu-ul li', { delay: 1000,origin:'right',interval:100})
+ScrollReveal().reveal('.profile-card-2', { delay: 200 ,interval:200})
+ScrollReveal().reveal('#members', { delay: 150 })
+
 
 
